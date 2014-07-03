cabb2d36be173cbe2987b13f22cc7a4419513624
thefonso <alephmage@gmail.com>
2014-07-02 17:38:59 -0700

    first commit

---
+++ basic_apps/hello_ruby.rb
@@ -0,0 +1,13 @@
+# run me by using cd to get to my directory
+# then type $ ruby hello_ruby.rb
+
+puts "Hello reader."
+puts "Welcome to Ruby"
+
+puts "Lets demonstrate a simple calculation"
+answer = 2 + 2
+puts "2 plus 2 is #{answer}"
+# >> Hello reader.
+# >> Welcome to Ruby
+# >> Lets demonstrate a simple calculation
+# >> 2 plus 2 is 4
---
+++ conditionals/02_elsif.rb
@@ -0,0 +1,18 @@
+# Introducing elsif
+
+		my_number = 56
+
+		if my_number < 12
+		  puts "Fewer than a dozen"
+		elsif my_number < 64
+		  puts "You could hold me in 6 bits"
+		else
+		  puts "That's a pretty big number"
+		end
+
+		# Output:
+  		# You could hold me in 6 bits
+  
+# NOTICE Ruby uses elsif, not 'else if' and not 'elif'.
+
+# Now we started with an if...else statement but let me show you something...
